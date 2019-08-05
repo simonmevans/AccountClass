664f6be8f3c36ad2111b09c20b702631f080938a
Simon Evans <simonmitchellevans@gmail.com>
2019-08-05 09:53:31 -0500

    first commit

---
+++ src/com/company/Account.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Account {
+    public String text = "hi folks";
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Account bob = new Account();
+        System.out.print(bob.text);
+    }
+}
