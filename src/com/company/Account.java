7eafa230564508c386e9fb0500d1589b087ee233
Simon Evans <simonmitchellevans@gmail.com>
2019-08-05 13:41:39 -0500

    finished program

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,5 +1,60 @@
 package com.company;
 
+import java.util.Date;
+
 public class Account {
-    public String text = "hi folks";
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    Account() {
+        id = 0;
+        balance = 0;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+
+    Account(int d, double b, double AIR) {
+        id = d;
+        balance = b;
+        annualInterestRate = AIR;
+        dateCreated = new Date();
+    }
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int num) {
+        id = num;
+    }
+
+    public Date getDateCreated ()
+    {
+        return dateCreated;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return (annualInterestRate/100) / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
 }
